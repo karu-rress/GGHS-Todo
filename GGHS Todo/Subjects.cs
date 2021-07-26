f306d7397ef02698706960e3a73db1d92737f52a
nsun527 <nsun527@naver.com>
2021-07-26 22:36:18 +0900

    Add project files.

---
+++ GGHS Todo/Subjects.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGHS
+{
+    interface ISubjects
+    {
+        int Grade { get; }
+    }
+    namespace Grade1
+    {
+        class Subjects : ISubjects
+        {
+            int ISubjects.Grade { get; } = 1;
+        }
+    }
+    namespace Grade2
+    {
+        class Subjects : ISubjects
+        {
+            int ISubjects.Grade { get; } = 2;
+        }
+    }
+    namespace Grade3
+    {
+        class Subjects : ISubjects
+        {
+            int ISubjects.Grade { get; } = 3;
+        }
+    }
+}
